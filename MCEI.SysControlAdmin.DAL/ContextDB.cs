7c08d44861b13f4561a02a5d5958b9b06e0b50d4
Hector Garcia <hg425823@gmail.com>
2024-08-14 21:54:30 -0600

    Actualizacion de ContextDB

--- MCEI.SysControlAdmin.DAL/ContextDB.cs
+++ MCEI.SysControlAdmin.DAL/ContextDB.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MCEI.SysControlAdmin.EN.Role___EN;
+
+using MCEI.SysControlAdmin.EN.User___EN;
+
 // Referencias Necesarias Para El Correcto Funcionamiento
 using Microsoft.EntityFrameworkCore;
 
@@ -17,7 +21,8 @@ namespace MCEI.SysControlAdmin.DAL
     {
         #region REFERENCIAS DE TABLAS DE LA BD
         //Coleccion que hace referencia a la tabla de la base de datos
-        
+        public DbSet<Role> Role { get; set; }
+        public DbSet<User> User { get; set; }
         #endregion
 
         // Metodo de Conexion a la Base de Datos
