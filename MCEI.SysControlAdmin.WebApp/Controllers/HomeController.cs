db1fd8e3b39708c1c78ac2429a098121191fc36c
Hector Garcia <hg425823@gmail.com>
2024-08-14 23:05:53 -0600

    Update de Controller Home

--- MCEI.SysControlAdmin.WebApp/Controllers/HomeController.cs
+++ MCEI.SysControlAdmin.WebApp/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 using MCEI.SysControlAdmin.WebApp.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace MCEI.SysControlAdmin.WebApp.Controllers
 {
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class HomeController : Controller
     {
+        [Authorize(Roles = "Desarrollador")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Desarrollador")]
+        public IActionResult Dashboard()
         {
             return View();
         }
