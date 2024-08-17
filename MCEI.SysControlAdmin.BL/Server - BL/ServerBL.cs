e2697f97b3e45e55bdd748e33fb56c01ce65966e
Hector Garcia <hg425823@gmail.com>
2024-08-17 12:30:03 -0600

    Insercion de clase ServerBL

---
+++ MCEI.SysControlAdmin.BL/Server - BL/ServerBL.cs
@@ -0,0 +1,72 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using MCEI.SysControlAdmin.EN.Server___EN;
+using MCEI.SysControlAdmin.DAL.Server___DAL;
+
+#endregion
+
+namespace MCEI.SysControlAdmin.BL.Server___BL
+{
+    public class ServerBL
+    {
+        #region METODO PARA CREAR
+        // Metodo Para Guardar Un Nuevo Registro
+        public async Task<int> CreateAsync(Server server)
+        {
+            return await ServerDAL.CreateAsync(server);
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Metodo Para Guardar Un Nuevo Registro
+        public async Task<int> UpdateAsync(Server server)
+        {
+            return await ServerDAL.UpdateAsync(server);
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Metodo Para Eliminar Un Registro Existente En La Base De Datos
+        public async Task<int> DeleteAsync(Server server)
+        {
+            return await ServerDAL.DeleteAsync(server);
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR
+        // Metodo Para Mostrar Una Lista De Registros
+        public async Task<List<Server>> GetAllAsync()
+        {
+            return await ServerDAL.GetAllAsync();
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR POR ID
+        // Metodo Para Mostrar Un Registro Especifico Bajo Un Id
+        public async Task<Server> GetByIdAsync(Server server)
+        {
+            return await ServerDAL.GetByIdAsync(server);
+        }
+        #endregion
+
+        #region METODO PARA BUSCAR
+        // Metodo Para Buscar Registros Existentes
+        public async Task<List<Server>> SearchAsync(Server server)
+        {
+            return await ServerDAL.SearchAsync(server);
+        }
+        #endregion
+
+        #region METODO PARA INCLUIR HORARIO E INSTRUCTOR
+        public async Task<List<Server>> SearchIncludeAsync(Server server)
+        {
+            return await ServerDAL.SearchIncludeAsync(server);
+        }
+        #endregion
+    }
+}
