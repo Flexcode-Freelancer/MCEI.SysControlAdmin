ad00085ecdd34a12205fe60c7c8853c914c53810
Hector Garcia <hg425823@gmail.com>
2024-08-24 12:16:00 -0600

    Creacion de Clase HistoryServerBL

---
+++ MCEI.SysControlAdmin.BL/HistoryServer - BL/HistoryServerBL.cs
@@ -0,0 +1,50 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using MCEI.SysControlAdmin.EN.HistoryServer___EN;
+using MCEI.SysControlAdmin.DAL.HistoryServer___DAL;
+
+
+#endregion
+
+namespace MCEI.SysControlAdmin.BL.HistoryServer___BL
+{
+    public class HistoryServerBL
+    {
+        #region METODO PARA CREAR
+        // Metodo Para Guardar Un Nuevo Registro
+        public async Task<int> CreateAsync(HistoryServer historyServer)
+        {
+            return await HistoryServerDAL.CreateAsync(historyServer);
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR
+        // Metodo Para Mostrar Una Lista De Registros
+        public async Task<List<HistoryServer>> GetAllAsync()
+        {
+            return await HistoryServerDAL.GetAllAsync();
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR POR ID
+        // Metodo Para Mostrar Un Registro Especifico Bajo Un Id
+        public async Task<HistoryServer> GetByIdAsync(HistoryServer historyServer)
+        {
+            return await HistoryServerDAL.GetByIdAsync(historyServer);
+        }
+        #endregion
+
+        #region METODO PARA BUSCAR
+        // Metodo Para Buscar Registros Existentes
+        public async Task<List<HistoryServer>> SearchAsync(HistoryServer historyServer)
+        {
+            return await HistoryServerDAL.SearchAsync(historyServer);
+        }
+        #endregion
+    }
+}
